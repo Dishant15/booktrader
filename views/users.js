f70995f4b110f525b8c9b048633d09eb0319b3e2
Dishant Chavda <dishantchavda@gmail.com>
2016-05-01 19:47:30 +0530

    added user profile both update password and detail functionality

--- views/users.js
+++ views/users.js
@@ -4,7 +4,14 @@ var passport = require('passport');
 
 var User = require('../models/user');
 
-/* GET users listing. */
+function loginRequired(req, res, next) {
+	if(req.user) next();
+	else {
+		req.flash('error_msg', 'Please login to proceed!!');
+		res.redirect('/users/login');
+	}
+}
+
 router.get('/login', function(req, res) {
     res.render('user/login', {
     	title: 'LogIn | BookTrader'
@@ -13,7 +20,7 @@ router.get('/login', function(req, res) {
 
 router.post('/login',
   passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/login',
+                                   failureRedirect: '/users/login',
                                    failureFlash: true })
 );
 
@@ -30,10 +37,7 @@ router.get('/signup', function(req, res) {
 });
 
 router.post('/signup', function(req, res) {
-	var username = req.body.username;
-    var email = req.body.email;
-    var password = req.body.password;
-
+	// validation checks
     req.checkBody('username', 'Username is required').notEmpty();
     req.checkBody('email', "Email is required").notEmpty();
     req.checkBody('email','Email is not valid').isEmail();
@@ -56,6 +60,53 @@ router.post('/signup', function(req, res) {
     		});
     	});
     }
+});
+
+// user profile module
+router.get('/profile', loginRequired, function(req, res){
+	res.render('user/profile', {
+		title: 'Profile | BookTrader'
+	})
+});
+
+// user profile module
+router.post('/update/details', loginRequired, function(req, res){
+	var user = req.user;
+	user.city = req.body.city || null;
+	user.state = req.body.state || null;
+	user.save(function(err, u){
+		if(err) throw err;
+		res.redirect('/users/profile');
+	});
+});
+
+// user profile module
+router.post('/update/password', loginRequired, function(req, res){
+	var loggeduser = req.user;
+
+	req.checkBody('curr_pass', 'Please enter your current password').notEmpty();
+	req.checkBody('new_pass', 'Please enter a new password').notEmpty();
+
+	var errors = req.validationErrors();
+	if(errors){
+		res.render('user/profile', {
+			title: 'Profile | BookTrader',
+			errors: errors
+		})
+	} else {
+		// validation passed change the password
+		User.comparePassword(req.body.curr_pass, loggeduser.password, function(err, isMatch){
+			if(isMatch){
+				loggeduser.password = req.body.new_pass;
+				User.createUser( loggeduser, function(err, u){
+					res.redirect('/users/profile');
+				});
+			} else {
+				req.flash('error_msg', 'Wrong current password!!');
+				res.redirect('/users/profile');
+			}
+		});
+	}
 });
 
 module.exports = router;
