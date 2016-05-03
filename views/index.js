3793a1d67e8e789d70112cd8878197dcc15af183
Dishant15 <dishantchavda@gmail.com>
2016-05-03 09:17:44 +0530

    starting with react templated

--- views/index.js
+++ views/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+	// var options = {
+	//     uri: 'https://www.googleapis.com/books/v1/volumes?q=the%20lost%20symbol',
+	//     method: 'GET',
+	//     json:true
+	// }
+	// request(options, function (error, response, body) {
+	// 	if(error) throw error;
+	// 	res.json(body);
+	// });
+	res.render('index');
+});
+
+router.get('/books/add', function(req, res){
+	res.render('add_book', {
+		title: "Add Book | BookTrader"
+	});
 });
 
 module.exports = router;
